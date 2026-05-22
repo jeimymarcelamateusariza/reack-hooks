fd7da04a33403c9e0b7d27224516ce8d1a2d11ff
Jeimy Marcela Mateus Ariza <jeimymateus1@gmail.com>
2026-05-21 19:49:28 -0500

    feat: add InstagromApp component with optimistic UI for comment handling and integrate sonner for notifications

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+
+import { Toaster, toast } from "sonner";
 // import HooksApp from "./HooksApp";
 // import { TrafficLight } from "./useState/TrafficLight";
 // import { TrafficLightWithEffect } from "./useEffect/TrafficLightWhitEffect";
@@ -9,9 +11,14 @@ import "./index.css";
 // import { PokemonPage } from "./examples/PokemonPage";
 //import { TasksApp } from "./useReducer/TaskApp";
 //import { FocusScreen } from "./useRef/FocusScreen";
-import { ScrambleWords } from "./useReducer/ScrambleWords";
+// import { ScrambleWords } from "./useReducer/ScrambleWords";
+// import MemoHook from "./memos/MemoHook";
+// import MemoCounter from "./memos/MemoCounter";
+import { InstagromApp } from "./useOptimistic/InstagromApp";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <Toaster />
     {/* <HooksApp /> */}
     {/* <TrafficLight /> */}
     {/* <TrafficLightWithEffect /> */}
@@ -20,6 +27,9 @@ createRoot(document.getElementById("root")!).render(
     {/* <PokemonPage /> */}
     {/* <FocusScreen /> */}
     {/* <TasksApp /> */}
-    <ScrambleWords />
+    {/* <ScrambleWords /> */}
+    {/* <MemoHook /> */}
+    {/* <MemoCounter /> */}
+    <InstagromApp />
   </StrictMode>,
 );
